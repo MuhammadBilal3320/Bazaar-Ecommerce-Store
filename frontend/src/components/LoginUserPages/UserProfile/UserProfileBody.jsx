801cafd49d84d93824d591391a523e82294e2687
Muhammad Bilal <bilal.x3320@gmail.com>
2025-03-10 13:32:30 +0500

    User Profile Starting Page Commit

--- frontend/src/components/LoginUserPages/UserProfile/UserProfileBody.jsx
+++ frontend/src/components/LoginUserPages/UserProfile/UserProfileBody.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const UserProfileBody = () => {
     return (
-        <div>
-            <h1>User Profile Body</h1>
-        </div>
+        <MainContainer>
+            <h1 className='ProfileHeading'>My Profile</h1>
+        </MainContainer>
     )
 }
 
 export default UserProfileBody
+
+const MainContainer = styled.main`
+    width: 100%;
+
+    .ProfileHeading{
+        background-color: whitesmoke;
+        padding: 1rem;
+        font-weight: 600;
+    }
+`;
